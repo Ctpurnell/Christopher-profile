84196c2217abd2665f5fb5c7f522b7214e276778
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-22 17:35:44 -0400

    getting more set up

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ function App() {
   return (
     <div className="container">
       <h1>{title}</h1>
-      <div>
+      <div>  
         <img
           style={{ width: "160px", height: "160px", borderRadius: "80px" }}
           src={pic}
           alt="picture of my face"
         />
       </div>
-      <h2>{"Welcome to my profile page. Yes all are welcome!"}</h2>
 
       <a href={link}>My GitHub Profile</a>
     </div>
