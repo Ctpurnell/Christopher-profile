27e16fde0055de8185ffee6929baa7a9ad49b774
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-22 16:25:07 -0400

    adding a pic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   const title = "Christopher Purnell";
   const link = "https://github.com/Ctpurnell";
-
-  
+  const pic = "/public/assets/IMG_0112.png"
   return (
-    <div className="App">
-      <div className="content">
-        <h1>{ title }</h1>
-        <p>{"Welcome to my profile page"}</p>
-
-        <a href={ link }>My GitHub Profile</a>
+    <div className="container">
+      <h1>{title}</h1>
+      <div>
+        <img
+          style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+          src={pic}
+          alt="picture of my face"
+        />
       </div>
-      
+      <p>{"Welcome to my profile page"}</p>
+
+      <a href={link}>My GitHub Profile</a>
     </div>
   );
 }
