b1046033df5c745ba2295dcfb53fde7932227105
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-27 15:19:11 -0400

    added some pics and styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,34 +7,34 @@ export default function Portfolio () {
   <div className="col-sm-6 mb-3 mb-sm-0">
     <div className="card lazy-dater">
       <div className="card-body">
-        <h5 className="card-title"> The Lazy Dater</h5>
+        <h2 className="card-title"> The Lazy Dater</h2>
         <p className="card-text">This was the first group project I did, and I must say it is my favorite.</p>
         <a href="#" className="btn btn-success">Check it Out!</a>
       </div>
     </div>
   </div>
   <div className="col-sm-6">
-    <div className="card">
+    <div className="card cartoonify">
       <div className="card-body">
-        <h5 className="card-title">Cartoonify Your Pic</h5>
+        <h2 className="card-title">Cartoonify Your Pic</h2>
         <p className="card-text">Another group project</p>
         <a href="#" className="btn btn-success">Check it Out!</a>
       </div>
     </div>
   </div>
   <div className="col-sm-6 mb-3 mb-sm-0">
-    <div className="card">
+    <div className="card jate-thing">
       <div className="card-body">
-        <h5 className="card-title">Progressive Web App</h5>
+        <h2 className="card-title">Progressive Web App</h2>
         <p className="card-text">J.A.T.E. is just another text editor I deployed to Heroku.</p>
         <a href="#" className="btn btn-success">Check it Out!</a>
       </div>
     </div>
   </div>
   <div className="col-sm-6 mb-3 mb-sm-0">
-    <div className="card">
+    <div className="card employee-track">
       <div className="card-body">
-        <h5 className="card-title">Employee Tracker</h5>
+        <h2 className="card-title">Employee Tracker</h2>
         <p className="card-text">Working with MySQL, this .</p>
         <a href="#" className="btn btn-success">Check it Out!</a>
       </div>
