fe15790f04657492b1c97697ee293974214263d1
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-27 17:44:31 -0400

    trying to add functionality for form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,12 @@ export default function Contact() {
   <form>
     <div className="form-group col-md-12 p-3">
       <label className="nameLabel row p-2 " htmlFor="name">Name:</label>
-      <input type="text" className="form-control border border-dark" id="name" placeholder="Enter your name" />
+      <input type="text" className="form-control border border-dark" id="name" required placeholder="Enter your name" />
     </div>
+
     <div className="form-group col-md-12 p-3">
       <label className="nameLabel row p-2" htmlFor="email">Email:</label>
-      <input type="email" className="form-control border border-dark" id="email" placeholder="Enter your email" />
+      <input type="email" className="form-control border border-dark" id="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="Enter your email" />
     </div>
 
     <div className="form-group col-md-12 p-3">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,18 @@ export default function Footer() {
 
     return (
         
-        <footer className="footer">
+        <footer className="bg-success text-center">
             <div>
-              <a className="centered" href="https://github.com"><i className="fab fa-github"></i>GitHub</a>
+              <a className="centered" href="https://github.com/Ctpurnell"><i className="fab fa-github"></i>GitHub</a>
             </div>
 
             <div>
-              <a className="centered" href="https://www.facebook.com"><i className="fab fa-facebook"></i>Facebook</a>
+              <a className="centered" href="https://www.facebook.com/christopher.purnell.9"><i className="fab fa-facebook"></i>Facebook</a>
             </div>
+
+            <div>
+              <a className="centered" href="https://www.linkedin.com/in/christopher-purnell-69280142/"><i className="fab fa-linked-in"></i>Linkedin</a>
+        </div>
         </footer>
 
     )
