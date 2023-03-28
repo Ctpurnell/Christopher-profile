34c4e6bda6f540b915b0460f1a94aa2a1721a876
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-27 20:28:15 -0400

    readme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
               style={{ width: "160px", height: "160px", borderRadius: "80px", padding: "5px"}}
               src={require("../IMG_0112.png")} alt="picture of my face"/>
             </div>
-            <div className="my-info">This is where I want my text and to manipulate the size of it I will use CSS</div>
+            <div className="my-info text-white">This is where I want my text and to manipulate the size of it I will use CSS</div>
         </div>
 
 
