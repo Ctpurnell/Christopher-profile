9a2b84dc4bb92307b6cf07e6e48c797ee092dcf9
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-03-27 21:20:44 -0400

    Dependencies

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const [currentPage, setCurrentPage] = useState('About-Me');
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  
   const renderPage = () => {
     if (currentPage === 'About-Me') {
       return <AboutMe />;
